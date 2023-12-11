cac9d26bdca0f8f7eda54b2a72e7e52b5a38a626
developerekene <developerekene@gmail.com>
2023-12-11 15:32:13 +0000

    Version one ready

--- src/App/UI/Components/PrimaryInputField/PrimaryInputField.tsx
+++ src/App/UI/Components/PrimaryInputField/PrimaryInputField.tsx
@@ -6,22 +6,51 @@ import "../PrimaryInputField/PrimaryInputField.css";
 type Props = React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
-> & {};
+> & {
+  fFamily?: string;
+  fWeight?: number;
+  color?: string;
+  w?: string;
+  h?: number;
+  mTop?: number;
+  mBottom?: number;
+  mAll?: number;
+  mRight?: number;
+  bRadius?: number;
+  pAll?: number;
+  pLeft?: number;
+  pRight?: number;
+  bColor?: string;
+  bWidth?: number;
+};
 
 const PrimaryInputField = (props: Props) => {
   return (
-    <label className="label">
+    <label style={{ fontFamily: props.fFamily, fontWeight: props.fWeight }}>
       {props.title && (
         <div>
           {props.title}
           {props.required ? (
-            <span className="required"> *</span>
+            <span style={{ color: "red" }}> *</span>
           ) : (
-            <span className="optional"> (Optional)</span>
+            <span style={{ fontWeight: props.fWeight }}> (Optional)</span>
           )}
         </div>
       )}
-      <input {...props} className="input" />
+      <input
+        {...props}
+        className="input"
+        style={{
+          width: props.w,
+          height: props.h,
+          marginTop: props.mTop,
+          borderRadius: props.bRadius,
+          paddingLeft: props.pLeft,
+          marginBottom: props.mBottom,
+          borderColor: props.bColor,
+          borderWidth: props.bWidth,
+        }}
+      />
     </label>
   );
 };
